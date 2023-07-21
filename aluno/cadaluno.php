08dcb66f818f30c086fd26e8163e9acdad3b168c
Ítalo Rangel <130422427+italorangel@users.noreply.github.com>
2023-07-20 22:50:57 -0300

    Add files via upload

---
+++ aluno/cadaluno.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_cadaluno.css">
+    <title>Document</title>
+</head>
+<body>
+  
+<div class="caixacadalu">
+
+   <form method="GET" action="crudaluno.php">
+
+     <div class="umm">
+
+       <label for="">nome aluno</label>
+     <input type="text" class="inp" name="nome">
+
+     <label for="">idade</label>
+     <input type="text" class="inp" name="idade"> 
+
+     <label for="">data de nascimento</label>
+     <input type="date" class="inp" name="datanascimento">
+
+  </div>
+
+  <div class="doiss">
+
+  
+    <label for="">endereço</label>
+     <input type="text" class="inp" name="endereco">
+
+     <label for="">estatus</label>
+     <input type="text" class="inp" name="estatus">
+
+     <input type="submit" name="cadastrar" id="cada" class="btn" value="CADASTRAR">
+
+     <button class="btn"><a href="http://localhost/atividade_17_07/index.php">voltar</a></button>
+
+  </div>
+
+
+     </form>
+
+   
+
+</div>
+   
+
+
+</body>
+</html>
