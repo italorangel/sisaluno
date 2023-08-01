924ec9a67ec0b3faf0f4b59bd289fa7df6bce6b4
Ítalo Rangel <130422427+italorangel@users.noreply.github.com>
2023-08-01 11:21:32 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="style.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="caixa">
+
+<div class="uma">
+    
+    <div class="titulo">SISTEMA DE CADASTRO</div>
+
+    <div class="alutitulo" >ALUNOS</div>
+
+    <div class="botn"> <button> <a href="./aluno/cadaluno.php">CADASTRAR</a></button> </div>
+    <div class="botn"> <button><a href="./aluno/listaalunos.php">LISTA</a> </button></div><br>
+
+    <div class="proftex">PROFESSOR</div> 
+
+    <div class="botn"> <button> <a href="./professor/cadprof.php">CADASTRAR</a></button> </div>
+    <div class="botn"> <button><a href="./professor/listaprof.php">LISTA</a> </button></div><br>
+
+    <div class="distex">DISCIPLINAS</div>
+
+    <div class="botn"> <button> <a href="./disciplinas/caddis.php">CADASTRAR</a></button> </div>
+    <div class="botn"> <button><a href="./disciplinas/listadis.php">LISTA</a> </button></div><br>
+
+
+</div>
+
+<div class="dois"> <img src="imagem1.jpg" alt=""></div>
+
+
+</div>
+
+   
+
+
+
+ 
+
+    
+
+
+   
+  
+
+</body>
+</html>
